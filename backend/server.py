f2f7b1a382e8fe9049cb23d20878197ca9699aa6
Shrijani08 <shrijanivaikunth@gmail.com>
2025-10-25 21:03:16 +0530

    server code change

--- backend/server.py
+++ backend/server.py
@@ -24,10 +24,10 @@ from keras.preprocessing import image
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-# MongoDB connection
-mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+# # MongoDB connection
+# mongo_url = os.environ['MONGO_URL']
+# client = AsyncIOMotorClient(mongo_url)
+# db = client[os.environ['DB_NAME']]
 
 # Create the main app
 app = FastAPI()
